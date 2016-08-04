f1a54aca237ac3df9d5cff4c2640639042b0e10f
Rômulo Cerqueira <romulogcerqueira@gmail.com>
2016-08-04 10:46:09 -0300

    Add new methods to ImageUtils
    - vector to mat
    - clahe 8u
    - estimate clahe parameters
    - insonification correction
    - stddev filter mask

--- src/ImageUtils.hpp
+++ src/ImageUtils.hpp
@@ -21,10 +21,64 @@ std::vector<T> mat2vector(cv::Mat mat) {
     return array;    
 }
 
+cv::Mat vector32f_to_mat8u(const std::vector<float>& src, int beam_count, int bin_count) {
+    cv::Mat dst(beam_count, bin_count, CV_32F, (void*) src.data());
+    dst.convertTo(dst, CV_8U, 255);
+    return dst;
+}
 
 void cv32f_equalize_histogram(cv::Mat src, cv::Mat dst);
 
 void cv32f_clahe(cv::Mat src, cv::Mat dst, double clip_size = 40.0, cv::Size grid_size = cv::Size(8, 8));
+
+/**
+ * This function minimizes the intensity offsets on the registered areas using the CLAHE technique
+ * (Contrast Limited Adaptive Histogram Equalization).
+ * @param src: the input image in 8-bit
+ * @param dst: the output image in 8-bit
+ * @param clip_size: real scalar that specifies a contrast enhancement limit. Higher numbers result in more contrast
+ * @param grid_size: size of grid for histogram equalization
+ */
+void clahe_mat8u(cv::Mat src, cv::Mat& dst, double clip_size = 40.0, cv::Size grid_size = cv::Size(8, 8));
+
+/**
+ * This function calculates the clahe parameters based on image entropy
+ * @param src: the input image as cv::Mat
+ * @param clip_limit: the best clip limit
+ * @param grid_size: the best grid size
+ */
+void estimate_clahe_parameters(const cv::Mat& src, float& clip_limit, int& grid_size);
+
+/**
+ * This function calculate the insonification pattern by the mean of N sonar frames
+ * @param frames: the array of sonar frames
+ * @return the insonification pattern
+ */
+std::vector<float> generate_insonification_pattern(const std::vector<std::vector<float> >& frames);
+
+/**
+ * This function load the insonification pattern from a file
+ * @param file_path: the file path name
+ * @param pattern: the insonification pattern loaded
+ * @return if the insonification pattern was loaded successfully
+ */
+bool load_insonification_pattern(std::string file_path, std::vector<float>& pattern);
+
+/**
+ * This function applies an inhomogeneous insonfication pattern correction on each raw sonar data,
+ * originated by different sensitivity of the transducers accross the field of view.
+ * @param data: the sonar data to be handled
+ * @param pattern: the insonification pattern loaded
+ */
+void apply_insonification_correction(std::vector<float>& data, const std::vector<float>& pattern);
+
+/**
+ * This function create a mask based on standard deviation
+ * @param src: the sonar image as cv::Mat
+ * @param mask_size: the mask size used (must be odd and equal or greater than 3)
+ * @return the mask generated
+ */
+cv::Mat create_stddev_filter_mask(const cv::Mat& src, uint mask_size);
 
 cv::Mat zeros_cols(cv::Mat src, std::vector<uint32_t> cols);
 
