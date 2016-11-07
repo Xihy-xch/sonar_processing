99963418ea03c0244d43bff4ade98f4be0f4f045
Rômulo Cerqueira <romulogcerqueira@gmail.com>
2016-11-07 15:17:43 -0300

    fix standard rls algorithm
    fix standard rls algorithm

--- src/Denoising.cpp
+++ src/Denoising.cpp
@@ -31,18 +31,39 @@ void denoising::homomorphic_filter(cv::InputArray _src, cv::OutputArray _dst, in
 
 
 /* Recursive Least Square Filter */
-void denoising::rls(cv::Mat& rls_p, cv::Mat& rls_w, cv::Mat& src, cv::Mat& dst) {
+void denoising::rls(cv::Mat& rls_w, cv::Mat& rls_p, cv::Mat& src, cv::Mat& dst) {
     // recover data
     if (src.depth() == CV_8U) src.convertTo(src, CV_32F, 1.0 / 255.0);
     src.copyTo(dst);
 
     if (rls_p.empty() || rls_w.empty()) {
-        rls_p = cv::Mat::ones(src.size(), CV_32F) * 1.5;
-        rls_w = cv::Mat::ones(src.size(), CV_32F) * 0.5;
+        rls_w = cv::Mat::ones(src.size(), CV_32F) * 0.4;
+        rls_p = cv::Mat::ones(src.size(), CV_32F) * 0.75;
     }
-    // estimation of w
+    // estimation of w parameters and its covariance p
     else {
+        // convert image to linear space
+        cv::Mat rls_d;
+        cv::log(src, rls_d);
 
+        for (size_t j = 0; j < rls_w.rows; j++) {
+            for (size_t i = 0; i < rls_w.cols; i++) {
+                float w = rls_w.at<float>(j, i);
+                float p = rls_p.at<float>(j, i);
+                float d = rls_d.at<float>(j, i);
+                p = p - (p * p) / (1 + p);
+                w = w + p * (d - w);
+
+                rls_p.at<float>(j, i) = p;
+                rls_w.at<float>(j, i) = w;
+            }
+        }
+
+        // convert back to nonlinear space
+        cv::exp(rls_w, dst);
+        cv::normalize(dst, dst, 0, 1, cv::NORM_MINMAX);
+    }
+}
         // convert image to logarithmic domain
         cv::Mat rls_d;
         cv::log(src, rls_d);
