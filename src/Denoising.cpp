a0dce8addf60455e5210df6629776e5317698794
Rômulo Cerqueira <romulogcerqueira@gmail.com>
2016-11-09 23:27:37 -0300

    RLS with dynamic window
    RLS with dynamic window

--- src/Denoising.cpp
+++ src/Denoising.cpp
@@ -29,7 +29,6 @@ void denoising::homomorphic_filter(cv::InputArray _src, cv::OutputArray _dst, in
     dst.convertTo(dst, CV_8UC1, 255);
 }
 
-
 /* Recursive Least Square Filter */
 void denoising::rls(cv::Mat& rls_w, cv::Mat& rls_p, cv::Mat& src, cv::Mat& dst) {
     // recover data
@@ -115,5 +114,60 @@ void denoising::rls_sliding_window(cv::Mat& rls_w, cv::Mat& rls_p, std::deque<cv
         cv::normalize(dst, dst, 0, 1, cv::NORM_MINMAX);
     }
     frames.push_back(src);
+}
+
+// /* Recursive Least Square Filter with Adaptative Window Control */
+void denoising::rls_adaptative_window(cv::Mat& rls_w, cv::Mat& rls_p, std::deque<cv::Mat>& frames, int window_size, cv::Mat& src, cv::Mat& dst) {
+
+    // if window size is less or equal than frames size, apply the rls sliding window
+    if (frames.size() <= window_size) {
+        rls_sliding_window(rls_w, rls_p, frames, window_size, src, dst);
+    }
+
+    // if the window size is decreased, apply downdate 2x and update 1x
+    else {
+        // recover data
+        if (src.depth() == CV_8U) src.convertTo(src, CV_32F, 1.0 / 255.0);
+
+        // convert image to linear domain
+        cv::Mat rls_d;
+        cv::log(src, rls_d);
+
+        // recover the two oldest data
+        cv::Mat first_oldest, second_oldest;
+        cv::log(frames.front(), first_oldest);
+        frames.pop_front();
+        cv::log(frames.front(), second_oldest);
+        frames.pop_front();
+
+        // per-wise process
+        for (size_t j = 0; j < rls_w.rows; j++) {
+            for (size_t i = 0; i < rls_w.cols; i++) {
+                float w = rls_w.at<float>(j, i);
+                float p = rls_p.at<float>(j, i);
+                float d = rls_d.at<float>(j, i);
+
+                // downdate 2x
+                float first_oldest_d = first_oldest.at<float>(j, i);
+                float second_oldest_d = second_oldest.at<float>(j, i);
+                p = p + ((p * p) / (1 - p));
+                w = w - p * (first_oldest_d - w);
+                p = p + ((p * p) / (1 - p));
+                w = w - p * (second_oldest_d - w);
+
+                // update 1x
+                p = p - ((p * p) / (1 + p));
+                w = w + p * (d - w);
+
+                rls_p.at<float>(j, i) = p;
+                rls_w.at<float>(j, i) = w;
+            }
+        }
+
+        // convert back
+        cv::exp(rls_w, dst);
+        cv::normalize(dst, dst, 0, 1, cv::NORM_MINMAX);
+        frames.push_back(src);
+    }
 }
 } /* namespace sonar_processing  */
--- src/Denoising.hpp
+++ src/Denoising.hpp
@@ -14,6 +14,8 @@ void homomorphic_filter(cv::InputArray _src, cv::OutputArray _dst, int iteration
 void rls(cv::Mat& rls_w, cv::Mat& rls_p, cv::Mat& src, cv::Mat& dst);
 
 void rls_sliding_window(cv::Mat& rls_w, cv::Mat& rls_p, std::deque<cv::Mat>& frames, int window_size, cv::Mat& src, cv::Mat& dst);
+
+void rls_adaptative_window(cv::Mat& rls_w, cv::Mat& rls_p, std::deque<cv::Mat>& frames, int window_size, cv::Mat& src, cv::Mat& dst);
 } /* namespace denoising */
 
 } /* namespace sonar_processing  */
