7dc04dc6907bb8aae20c9ce52d8372755fb66b7f
Rômulo Cerqueira <romulogcerqueira@gmail.com>
2016-11-17 14:20:26 -0300

    refactoring Denoising filters
    refactoring Denoising filters

--- src/Denoising.cpp
+++ src/Denoising.cpp
@@ -1,10 +1,134 @@
 #include "base/Plot.hpp"
 #include "sonar_processing/Denoising.hpp"
+#include "sonar_processing/QualityMetrics.hpp"
 
 namespace sonar_processing {
 
+namespace denoising {
+
+// Standard Recursive Least Square Filter algorithm
+void RLS::infinite_window(cv::InputArray _src, cv::OutputArray _dst) {
+    // recover data
+    cv::Mat src = _src.getMat();
+    if (src.depth() == CV_8U) src.convertTo(src, CV_32F, 1.0 / 255.0);
+
+    src.copyTo(_dst);
+    cv::Mat dst = _dst.getMat();
+
+    // initialize coefficients
+    if (rls_p.empty() || rls_w.empty()) {
+        rls_p = cv::Mat::ones(src.size(), CV_32F) * 0.5;
+        rls_w = cv::Mat::zeros(src.size(), CV_32F);
+    }
+
+    // estimation of w parameters and its covariance p
+    else {
+        // convert image to linear space
+        cv::Mat rls_d;
+        cv::log(src, rls_d);
+
+        for (size_t i = 0; i < rls_w.rows; i++) {
+            for (size_t j = 0; j < rls_w.cols; j++) {
+                float p = rls_p.at<float>(i, j);
+                float w = rls_w.at<float>(i, j);
+                float d = rls_d.at<float>(i, j);
+                p = p - ((p * p) / (1 + p));
+                w = w + p * (d - w);
+
+                rls_p.at<float>(i, j) = p;
+                rls_w.at<float>(i, j) = w;
+            }
+        }
+
+        // convert back to nonlinear space
+        cv::exp(rls_w, dst);
+        cv::normalize(dst, dst, 0, 1, cv::NORM_MINMAX);
+    }
+}
+
+// Recursive Least Square Filter algorithm with fixed data window
+void RLS::sliding_window(cv::InputArray _src, cv::OutputArray _dst) {
+    // recover data
+    cv::Mat src = _src.getMat();
+    if (src.depth() == CV_8U) src.convertTo(src, CV_32F, 1.0 / 255.0);
+
+    src.copyTo(_dst);
+    cv::Mat dst = _dst.getMat();
+
+    // check for downdate
+    if (frames.size() == window_size) {
+        cv::Mat oldest;
+        cv::log(frames.front(), oldest);
+        frames.pop_front();
+
+        // remove old measurement
+        for (size_t i = 0; i < rls_w.rows; i++) {
+            for (size_t j = 0; j < rls_w.cols; j++) {
+                float p = rls_p.at<float>(i, j);
+                float w = rls_w.at<float>(i, j);
+                float oldest_d = oldest.at<float>(i, j);
+                p = p + ((p * p) / (1 - p));
+                w = w - p * (oldest_d - w);
+
+                rls_p.at<float>(i, j) = p;
+                rls_w.at<float>(i, j) = w;
+            }
+        }
+    }
+
+    // update coefficients
+    infinite_window(src, dst);
+
+    // store current frame
+    frames.push_back(src.clone());
+}
+
+// Recursive Least Square Filter algorithm with dynamic data window size
+void RLS::adaptative_window(cv::InputArray _src, cv::OutputArray _dst) {
+
+    // recover data
+    cv::Mat src = _src.getMat();
+    if (src.depth() == CV_8U) src.convertTo(src, CV_32F, 1.0 / 255.0);
+
+    src.copyTo(_dst);
+    cv::Mat dst = _dst.getMat();
+
+    // if window size is higher than frame size, decrease 1x
+    if (frames.size() > window_size) {
+        cv::Mat oldest;
+        cv::log(frames.front(), oldest);
+        frames.pop_front();
+
+        // remove old measurement
+        for (size_t i = 0; i < rls_w.rows; i++) {
+            for (size_t j = 0; j < rls_w.cols; j++) {
+                float p = rls_p.at<float>(i, j);
+                float w = rls_w.at<float>(i, j);
+                float oldest_d = oldest.at<float>(i, j);
+                p = p + ((p * p) / (1 - p));
+                w = w - p * (oldest_d - w);
+
+                rls_p.at<float>(i, j) = p;
+                rls_w.at<float>(i, j) = w;
+            }
+        }
+    }
+
+    // run sliding window and update coefficients
+    sliding_window(src, dst);
+
+    // adaptative window size
+    double mse_i = qs::MSE(src, dst);
+    if (!mse_0 && mse_i && frames.size() == window_size) mse_0 = mse_i;
+
+    if (mse_0) {
+        if ((mse_i <= mse_0) && (window_size < 10)) window_size++;
+        if ((mse_i > mse_0) && (window_size > 2)) window_size--;
+    }
+}
+
 // Homomorphic Filtering with the log function
-void denoising::homomorphic_filter(cv::InputArray _src, cv::OutputArray _dst, int iterations) {
+void homomorphic_filter(cv::InputArray _src, cv::OutputArray _dst, int iterations) {
     cv::Mat src = _src.getMat();
     cv::Mat mat;
     src.copyTo(mat);
@@ -29,145 +153,6 @@ void denoising::homomorphic_filter(cv::InputArray _src, cv::OutputArray _dst, in
     dst.convertTo(dst, CV_8UC1, 255);
 }
 
-/* Recursive Least Square Filter */
-void denoising::rls(cv::Mat& rls_w, cv::Mat& rls_p, cv::Mat& src, cv::Mat& dst) {
-    // recover data
-    if (src.depth() == CV_8U) src.convertTo(src, CV_32F, 1.0 / 255.0);
-    src.copyTo(dst);
+} /* namespace denoising */
 
-    if (rls_p.empty() || rls_w.empty()) {
-        rls_w = cv::Mat::ones(src.size(), CV_32F) * 0.4;
-        rls_p = cv::Mat::ones(src.size(), CV_32F) * 0.75;
-    }
-    // estimation of w parameters and its covariance p
-    else {
-        // convert image to linear space
-        cv::Mat rls_d;
-        cv::log(src, rls_d);
-
-        for (size_t j = 0; j < rls_w.rows; j++) {
-            for (size_t i = 0; i < rls_w.cols; i++) {
-                float w = rls_w.at<float>(j, i);
-                float p = rls_p.at<float>(j, i);
-                float d = rls_d.at<float>(j, i);
-                p = p - (p * p) / (1 + p);
-                w = w + p * (d - w);
-
-                rls_p.at<float>(j, i) = p;
-                rls_w.at<float>(j, i) = w;
-            }
-        }
-
-        // convert back to nonlinear space
-        cv::exp(rls_w, dst);
-        cv::normalize(dst, dst, 0, 1, cv::NORM_MINMAX);
-    }
-}
-
-/* Recursive Least Square Filter with Finite Window */
-void denoising::rls_sliding_window(cv::Mat& rls_w, cv::Mat& rls_p, std::deque<cv::Mat>& frames, int window_size, cv::Mat& src, cv::Mat& dst) {
-    // recover data
-    if (src.depth() == CV_8U) src.convertTo(src, CV_32F, 1.0 / 255.0);
-    src.copyTo(dst);
-
-    if (rls_w.empty() || rls_p.empty()) {
-        rls_w = cv::Mat::ones(src.size(), CV_32F) * 0.4;
-        rls_p = cv::Mat::ones(src.size(), CV_32F) * 0.75;
-    }
-    // estimation of w parameters and its covariance p
-    else {
-        // convert image to logarithmic domain
-        cv::Mat rls_d;
-        cv::log(src, rls_d);
-
-        bool downdate = (frames.size() == window_size);
-        cv::Mat oldest;
-        if (downdate) {
-            cv::log(frames.front(), oldest);
-            frames.pop_front();
-        }
-
-        for (size_t j = 0; j < rls_w.rows; j++) {
-            for (size_t i = 0; i < rls_w.cols; i++) {
-                float w = rls_w.at<float>(j, i);
-                float p = rls_p.at<float>(j, i);
-                float d = rls_d.at<float>(j, i);
-
-                // downdate
-                if (downdate) {
-                    float oldest_d = oldest.at<float>(j, i);
-                    p = p + ((p * p) / (1 - p));
-                    w = w - p * (oldest_d - w);
-                }
-
-                // update
-                p = p - ((p * p) / (1 + p));
-                w = w + p * (d - w);
-
-                rls_p.at<float>(j, i) = p;
-                rls_w.at<float>(j, i) = w;
-            }
-        }
-
-        // convert back
-        cv::exp(rls_w, dst);
-        cv::normalize(dst, dst, 0, 1, cv::NORM_MINMAX);
-    }
-    frames.push_back(src);
-}
-
-// /* Recursive Least Square Filter with Adaptative Window Control */
-void denoising::rls_adaptative_window(cv::Mat& rls_w, cv::Mat& rls_p, std::deque<cv::Mat>& frames, int window_size, cv::Mat& src, cv::Mat& dst) {
-
-    // if window size is less or equal than frames size, apply the rls sliding window
-    if (frames.size() <= window_size) {
-        rls_sliding_window(rls_w, rls_p, frames, window_size, src, dst);
-    }
-
-    // if the window size is decreased, apply downdate 2x and update 1x
-    else {
-        // recover data
-        if (src.depth() == CV_8U) src.convertTo(src, CV_32F, 1.0 / 255.0);
-
-        // convert image to linear domain
-        cv::Mat rls_d;
-        cv::log(src, rls_d);
-
-        // recover the two oldest data
-        cv::Mat first_oldest, second_oldest;
-        cv::log(frames.front(), first_oldest);
-        frames.pop_front();
-        cv::log(frames.front(), second_oldest);
-        frames.pop_front();
-
-        // per-wise process
-        for (size_t j = 0; j < rls_w.rows; j++) {
-            for (size_t i = 0; i < rls_w.cols; i++) {
-                float w = rls_w.at<float>(j, i);
-                float p = rls_p.at<float>(j, i);
-                float d = rls_d.at<float>(j, i);
-
-                // downdate 2x
-                float first_oldest_d = first_oldest.at<float>(j, i);
-                float second_oldest_d = second_oldest.at<float>(j, i);
-                p = p + ((p * p) / (1 - p));
-                w = w - p * (first_oldest_d - w);
-                p = p + ((p * p) / (1 - p));
-                w = w - p * (second_oldest_d - w);
-
-                // update 1x
-                p = p - ((p * p) / (1 + p));
-                w = w + p * (d - w);
-
-                rls_p.at<float>(j, i) = p;
-                rls_w.at<float>(j, i) = w;
-            }
-        }
-
-        // convert back
-        cv::exp(rls_w, dst);
-        cv::normalize(dst, dst, 0, 1, cv::NORM_MINMAX);
-        frames.push_back(src);
-    }
-}
-} /* namespace sonar_processing  */
+} /* namespace sonar_processing */
--- src/Denoising.hpp
+++ src/Denoising.hpp
@@ -9,16 +9,45 @@ namespace sonar_processing {
 
 namespace denoising {
 
+// Recursive Least Squares Filter
+class RLS {
+
+public:
+    RLS()
+        : rls_w()
+        , rls_p()
+        , frames()
+        , window_size(8)
+        , mse_0(0)
+        {};
+
+    RLS(unsigned int _window_size)
+        : rls_w()
+        , rls_p()
+        , frames()
+        , window_size(_window_size)
+        , mse_0(0)
+        {};
+
+    ~RLS(){};
+
+    void infinite_window(cv::InputArray _src, cv::OutputArray _dst);
+    void sliding_window(cv::InputArray _src, cv::OutputArray _dst);
+    void adaptative_window(cv::InputArray _src, cv::OutputArray _dst);
+    void setWindow_size(uint value) { window_size = value; };
+    uint getWindow_size() { return window_size; };
+
+protected:
+    cv::Mat rls_w, rls_p;
+    std::deque<cv::Mat> frames;
+    uint window_size;
+    double mse_0;
+};
+
 void homomorphic_filter(cv::InputArray _src, cv::OutputArray _dst, int iterations);
 
-void rls(cv::Mat& rls_w, cv::Mat& rls_p, cv::Mat& src, cv::Mat& dst);
-
-void rls_sliding_window(cv::Mat& rls_w, cv::Mat& rls_p, std::deque<cv::Mat>& frames, int window_size, cv::Mat& src, cv::Mat& dst);
-
-void rls_adaptative_window(cv::Mat& rls_w, cv::Mat& rls_p, std::deque<cv::Mat>& frames, int window_size, cv::Mat& src, cv::Mat& dst);
 } /* namespace denoising */
 
-} /* namespace sonar_processing  */
-
+} /* namespace sonar_processing */
 
 #endif /* sonar_processing_SonarROI_hpp */
