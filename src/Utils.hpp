310f88157a85ae25d9bf98e8af8f0792dda27609
gustavoneves12 <gustavo.neves12@gmail.com>
2017-01-05 16:11:41 -0300

    add new utils functions

--- src/Utils.hpp
+++ src/Utils.hpp
@@ -5,10 +5,20 @@
 #include <stdint.h>
 #include <vector>
 #include <sys/time.h>
+#include <opencv2/opencv.hpp>
 
 namespace sonar_processing {
 
 namespace utils {
+
+inline cv::Rect clip_rect(const cv::Point& tl, const cv::Point& br, const cv::Point& min_tl, const cv::Point& max_br) {
+    return cv::Rect(cv::Point(std::max(min_tl.x, tl.x), std::max(min_tl.y, tl.y)), 
+                    cv::Point(std::min(br.x, max_br.x), std::min(br.y, max_br.y)));
+}
+
+inline cv::Rect neighborhood_rect(int x, int y, int n, int w, int h) {
+    return clip_rect(cv::Point(x-n, y-n), cv::Point(x+n, y+n), cv::Point(0, 0), cv::Point(w-1, h-1));
+}
 
 inline double clip(double val, double min, double max) {
     return (val < min) ? min : ((val > max) ? max : val);
@@ -16,6 +26,13 @@ inline double clip(double val, double min, double max) {
 
 inline uint32_t border_fit(uint32_t x,  uint32_t total_size, uint32_t block_size) {
     return (total_size > x + block_size) ? x : total_size - block_size - 1;
+}
+
+inline void point2f_to_point2i(const std::vector<cv::Point2f>& pts2f, std::vector<cv::Point>& pts2i) {
+    pts2i.assign(pts2f.size(), cv::Point(-1, -1));
+    for (size_t k = 0; k < pts2f.size(); k++) {
+        pts2i[k] = cv::Point2i((int)roundf(pts2f[k].x), (int)roundf(pts2f[k].y));
+    }
 }
 
 template <typename T>
@@ -37,10 +54,9 @@ inline static uint64_t milliseconds() {
     return microseconds() / 1000;    
 }
 
-}
+} /* namespace now */
 
-
-} /* end of namespace utilities */
+} /* end of namespace utils */
 
 } /* end of namespace sonar_processing */
 
