c63dafe5b7bdd781399fc92e59b0f4b532c3051f
Rômulo Cerqueira <romulogcerqueira@gmail.com>
2017-04-22 17:43:59 -0300

    add ScanningHolder to handle singlebeam sonar data
    add ScanningHolder to handle singlebeam sonar data

---
+++ src/ScanningHolder.cpp
@@ -0,0 +1,101 @@
+#include "ScanningHolder.hpp"
+
+namespace sonar_processing {
+
+// update the obstacle detection with current sonar frame
+void ScanningHolder::update(const base::samples::Sonar& sonar) {
+    if (!sonar.bin_count || !(sonar.beam_count == 1))
+        return;
+
+    // check if the motor step size is changed
+    bool changedMotorStep = last_sonar_.beam_count && isMotorStepChanged(sonar.bearings[0]);
+
+    if ((changedMotorStep || sonar.bin_count != last_sonar_.bin_count) && motor_step_.rad && last_sonar_.beam_count) {
+
+        // set the transfer vector between image pixels and sonar data
+        generateTransferTable(sonar);
+
+        // resets the accumulated sonar data
+        if (changedMotorStep || sonar.bin_count != last_sonar_.bin_count)
+            accum_data_.assign(num_steps_ * sonar.bin_count, 0.0);
+    }
+
+    // add the current sonar data
+    if (accum_data_.size()) {
+        int id_beam = round((num_steps_ - 1) * (sonar.bearings[0].rad + M_PI) / (2 * M_PI));
+
+        for (size_t i = 0; i < sonar.bin_count; ++i)
+            accum_data_[id_beam * sonar.bin_count + i] = sonar.bins[i];
+    }
+
+    drawSonarData();
+    last_sonar_ = sonar;
+}
+
+// draw the sector scanning data
+void ScanningHolder::drawSonarData() {
+    for (size_t i = 0; i < transfer_.size(); ++i) {
+        if (transfer_[i] != -1) {
+            cart_image_.at<float>(i / cart_image_.cols, i % cart_image_.cols) = accum_data_[transfer_[i]];
+        }
+    }
+}
+
+// check is the motor step angle size is changed
+bool ScanningHolder::isMotorStepChanged(const base::Angle& bearing) {
+    base::Angle diff_step = bearing - last_sonar_.bearings[0];
+    diff_step.rad = fabs(diff_step.rad);
+
+    // if the sector scanning is enabled, the diffStep could be lower than motorStep when the bearing is closer to one of the corners
+    if (fabs((left_limit_ - bearing).rad) < motor_step_.rad || fabs((right_limit_ - bearing).rad) < motor_step_.rad) {
+        last_diff_step_ = diff_step;
+        return false;
+    }
+
+    if (!motor_step_.isApprox(diff_step) && last_diff_step_.isApprox(diff_step)) {
+        motor_step_ = diff_step;
+        num_steps_ = M_PI * 2 / motor_step_.rad;
+        last_diff_step_ = diff_step;
+        return true;
+    }
+
+    last_diff_step_ = diff_step;
+    return false;
+}
+
+// set the transfer vector between image pixels and sonar data
+void ScanningHolder::generateTransferTable(const base::samples::Sonar& sonar) {
+    transfer_.clear();
+    cart_image_.setTo(0);
+    cart_mask_.setTo(0);
+
+    if (!motor_step_.rad)
+        return;
+
+    // set the origin
+    cv::Point2f origin(cart_image_.cols / 2, cart_image_.rows / 2);
+
+    for (size_t j = 0; j < cart_image_.rows; j++) {
+        for (size_t i = 0; i < cart_image_.cols; i++) {
+            // current point
+            cv::Point2f point(i - origin.x, j - origin.y);
+            point.x = point.x * (float) sonar.bin_count / (cart_image_.cols * 0.5);
+            point.y = point.y * (float) sonar.bin_count / (cart_image_.rows * 0.5);
+
+            double radius = sqrt(point.x * point.x + point.y * point.y);
+            double angle = atan2(point.x, -point.y);
+
+            // pixels out the sonar image
+            if(radius > sonar.bin_count || !radius || angle < left_limit_.rad || angle > right_limit_.rad)
+                transfer_.push_back(-1);
+
+            // pixels in the sonar image
+            else {
+                cart_mask_.at<uchar>(i,j) = 255;
+                int id_beam = round((num_steps_ - 1) * (angle + M_PI) / (2 * M_PI));
+                transfer_.push_back(id_beam * sonar.bin_count + radius);
+            }
+        }
+    }
+}
+} /* namespace sonar_processing */
---
+++ src/ScanningHolder.hpp
@@ -0,0 +1,107 @@
+#ifndef ScanningHolder_hpp
+#define ScanningHolder_hpp
+
+#include <stdio.h>
+
+// OpenCV includes
+#include <opencv2/opencv.hpp>
+
+// Rock includes
+#include <base/Angle.hpp>
+#include <base/samples/Sonar.hpp>
+
+namespace sonar_processing {
+
+class ScanningHolder {
+
+public:
+    ScanningHolder( uint width,
+                    uint height)
+                    : transfer_()
+                    , accum_data_()
+                    , motor_step_(base::Angle::fromRad(0))
+                    , last_diff_step_(base::Angle::fromRad(0))
+                    , num_steps_(0)
+                    , last_sonar_()
+    {
+        cart_image_ = cv::Mat::zeros(width, height, CV_32F);
+        cart_mask_  = cv::Mat::zeros(width, height, CV_8U);
+        left_limit_ = base::Angle::Min();
+        right_limit_ = base::Angle::Max();
+    };
+
+    ~ScanningHolder(){};
+
+    /**
+     * Updates the sonar view and local occupancy grid with the current sonar frame.
+     * @param sonar - the sonar data
+     */
+    void update(const base::samples::Sonar& sonar);
+
+    /**
+     * Plot the current sonar view as a cv::Mat.
+     * @return the sonar view
+     */
+    void drawSonarData();
+
+    /**
+     * Return the cartesian representation of current sonar data
+     * @return the cartesian image
+     */
+    cv::Mat getCartImage() const {
+        return cart_image_;
+    }
+
+    /**
+     * Return the cartesian representation of current sonar data
+     * @return the cartesian image
+     */
+    cv::Mat getCartMask() const {
+        return cart_mask_;
+    }
+
+    void addSonarData(const base::samples::Sonar& sonar);
+
+private:
+    /* the transfer vector between image pixels and sonar data */
+    std::vector<int> transfer_;
+
+    /* the accumulated data to be display on sonar view */
+    std::vector<float> accum_data_;
+
+    /* the view of accumulated sonar readings */
+    cv::Mat cart_image_;
+
+    /* the drawable area */
+    cv::Mat cart_mask_;
+
+    /* the sector scan */
+    base::Angle left_limit_, right_limit_;
+
+    /* the angular difference between two beams and the previous one */
+    base::Angle motor_step_, last_diff_step_;
+
+    /* the number of possible beams by the motor_step */
+    int num_steps_;
+
+    /* the previous sonar reading */
+    base::samples::Sonar last_sonar_;
+
+protected:
+    /**
+    * Checks if the motor step angle size changed.
+    * @param bearing - the current beam angle
+    * @return true or false
+    */
+    bool isMotorStepChanged(const base::Angle& bearing);
+
+    /**
+    * Correlates each pixel image with its corresponding sonar data.
+    * @param sonar - the sonar data
+    */
+    void generateTransferTable(const base::samples::Sonar& sonar);
+};
+
+} /* namespace sonar_processing */
+
+#endif /* sonar_processing_ScanningHolder_hpp */
